7fdd66ee6df7540499d7a6d86b2d650b2d625d3d
James Fisher <fisher.jc@gmail.com>
2015-08-19 16:15:47 -0400

    initial seeds file complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,16 @@ require 'faker'
 # Create Users
 10.times do
   user = User.new(
-    name: Faker::Name.name,
     email: Faker::Internet.email,
     password: Faker::Internet.password(8) 
     )
+    user.skip_confirmation!
+    user.save!
 end
 
 # Create Registered Applications
 25.times do
-  registered_application = Registered_Application.new(
+  registered_application = RegisteredApplication.create(
     name: Faker::App.name,
     url: Faker::Internet.url,
     user: User.all.sample
@@ -20,21 +21,17 @@ end
 
 # Create Events
 75.times do
-    event = Event.new(
+    event = Event.create(
         name: Faker::Hacker.noun,
         count: Faker::Number.between(1, 100),
-        registered_application: Registered_Application.all.sample
+        registered_application: RegisteredApplication.all.sample
         )
 end
 
-
-
 # Create an admin
 admin = User.new(
-    name:       'Admin User',
     email:      'admin@example.com',
-    password:   'helloworld',
-    role:       'admin'
+    password:   'helloworld'
 )
 admin.skip_confirmation!
 admin.save!
@@ -46,3 +43,8 @@ member = User.new(
 )
 member.skip_confirmation!
 member.save!
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{RegisteredApplication.count} registered applications created"
+puts "#{Event.count} events created"
